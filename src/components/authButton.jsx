49972b45bfd474306ff3c6e298abf52c3ea3a9f7
Alexander Hutchinson <ahutchin2003@gmail.com>
2024-12-19 15:07:01 -0500

    Implemented front-end back-end intergration for spotify authorization api call

--- src/components/authButton.jsx
+++ src/components/authButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AuthButton = ({ title, endpoint }) => {
   const handleOnClick = () => {
-    const baseUrl = process.env.REACT_APP_API_BASE_URL;
+    const baseUrl = import.meta.env.VITE_API_BASE_URL;
     window.location.href = `${baseUrl}${endpoint}`;
   };
 
